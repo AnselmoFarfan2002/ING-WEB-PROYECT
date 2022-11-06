994891dfa80da5c40087c71201e0f0503cad9a29
DiegoM0105 <diegomontero894@gmail.com>
2022-11-06 09:42:15 -0500

    Modificaciones respecto al registro de cuenta y en la base de datos

--- sql-scripts/usuario.funciones.sql
+++ sql-scripts/usuario.funciones.sql
@@ -3,6 +3,7 @@ use LOCAL_DB;
 -- SET GLOBAL log_bin_trust_function_creators = 1;
 drop function if exists validar_credenciales;
 drop function if exists validar_registro;
+drop function if exists get_emp_ruc;
 
 DELIMITER $$ 
 create function validar_credenciales( email varchar(40), pass varchar(40), keyword varchar(25) ) returns tinyint begin
@@ -22,42 +23,64 @@ create function validar_registro(email varchar(40), ruc varchar(11)) returns tin
 	-- failure: -1 
     -- successful: 1
     
-	if (select count(USU_RUC) from USUARIO where USU_RUC = ruc or UCASE(USU_CORREO) = UCASE(email)) > 0 then
-		return -1;
-	else 
+	if (select count(EMP_RUC) from EMPRESA where EMP_RUC = ruc) > 0 then
+		if (select count(USU_CORREO) from USUARIO where UCASE(USU_CORREO) = UCASE(email)) > 0 then
+			return -1;
+		else 
+			return 1;
+		end if;
+	else
 		return 1;
     end if;
 end;
  -- select validar_registro('jamesrod19@gmail.com','09876543121');
+ 
+DELIMITER $$
+create function get_emp_ruc() returns varchar(11) begin
+    return (select EMP_RUC FROM EMPRESA ORDER BY EMP_RUC DESC LIMIT 1);
+end$$
 
 /* ----------------------------------------------------------------------------------- */
 drop procedure if exists get_usu_contrasenia;
+drop procedure if exists post_emp_empresa;
 drop procedure if exists post_usu_usuario;
 
+DELIMITER $$
 create procedure get_usu_contrasenia( email varchar(40), keyword varchar(25) ) begin
 	select cast( aes_decrypt( USU_CONTRASENIA, keyword ) as char ) as pass from USUARIO where USU_CORREO = email;
-end;
+end$$
+
+DELIMITER $$
+create procedure post_emp_empresa( 
+	EMP_RUC varchar(11),
+	EMP_ENTIDAD varchar(30),
+    EMP_UBICACION varchar(40),
+    EMP_ROL_ENTIDAD varchar(20),
+    EMP_FOTO varchar(200)
+) begin
+	INSERT INTO EMPRESA VALUES (EMP_RUC, EMP_ENTIDAD, EMP_UBICACION, EMP_ROL_ENTIDAD, EMP_FOTO);
+end$$
 
 DELIMITER $$
 create procedure post_usu_usuario( 
-	USU_RUC varchar(11),
-	USU_ENTIDAD varchar(30),
-    USU_UBICACION varchar(40),
-    USU_ROL_ENTIDAD varchar(20),
     USU_NOMBRE varchar(30),
-    USU_ROL_PERSONA varchar(20),
+    USU_EMPRESA varchar(11),
+    USU_APELLIDO1 varchar(30),
+    USU_APELLIDO2 varchar(30),
+    USU_CARGO varchar(20),
     USU_CORREO varchar(40),
     USU_CELULAR varchar(13),
     USU_TELEFONO varchar(7),
     USU_CONTRASENIA blob,
-    keyword varchar(25)
+    keyword varchar(25),
+    USU_FOTO varchar(200)
 ) begin
-	INSERT INTO USUARIO VALUES (USU_RUC, USU_ENTIDAD, USU_UBICACION, USU_ROL_ENTIDAD, USU_NOMBRE, USU_ROL_PERSONA, UPPER(USU_CORREO), USU_CELULAR, USU_TELEFONO, aes_encrypt(USU_CONTRASENIA, keyword) );
+	INSERT INTO USUARIO (USU_NOMBRE, USU_EMPRESA, USU_APELLIDO1, USU_APELLIDO2, USU_CARGO, USU_CORREO, USU_CELULAR, USU_TELEFONO, USU_CONTRASENIA, USU_FOTO) VALUES (USU_NOMBRE, USU_EMPRESA, USU_APELLIDO1, USU_APELLIDO2, USU_CARGO, UPPER(USU_CORREO), USU_CELULAR, USU_TELEFONO, aes_encrypt(USU_CONTRASENIA, keyword), USU_FOTO);
 end$$
 
 -- delete from USUARIO where USU_RUC = 01234567891;
 -- call post_usu_usuario('01234567891','TakanaSoft','Tacna','Desarrolladora','Anselmo Farfan','Jefe de Área','anselmofarfan2002@gmail.com','984532631','','ASDasd123','unFzhzuOxruQMAjKavux59cF4');
 
-call get_usu_contrasenia('anselmofarfan2002@gmail.com','unFzhzuOxruQMAjKavux59cF4')
+-- call get_usu_contrasenia('anselmofarfan2002@gmail.com','unFzhzuOxruQMAjKavux59cF4')
 
 $$
--- src/controllers/module 1 - users accounts/registro_cuenta.js
+++ src/controllers/module 1 - users accounts/registro_cuenta.js
@@ -3,26 +3,34 @@ const controllers = {};
 
 controllers.REGISTRAR_CUENTA = (req, res) => {
     let query = mysqlConnection.format('select validar_registro(?,?) as status', [req.body.correo, req.body.ruc]);
+
     mysqlConnection.query(query, (err, rows, ifield)=>{
         let msg = ''; //Mensaje
         var i = 0; //Indice
-        let validate = [ //Expresiones regulares para campos de registro
+        let validate = [
             /^\d{11}$/, //Patron para RUC
             /^[a-zA-ZÀ-ÿ\00f1\00d1]+(\s*[a-zA-ZÀ-ÿ\00f1\00d1]*)*[a-zA-ZÀ-ÿ\00f1\00d1]{2,30}$/, //Patron para entidad
             /^[a-zA-ZÀ-ÿ\00f1\00d1]+(\s*[a-zA-ZÀ-ÿ\00f1\00d1]*)*[a-zA-ZÀ-ÿ\00f1\00d1]{2,40}$/, //Patron para ubicacion
             /^[a-zA-ZÀ-ÿ\00f1\00d1]+(\s*[a-zA-ZÀ-ÿ\00f1\00d1]*)*[a-zA-ZÀ-ÿ\00f1\00d1]{2,20}$/, //Patron para rol de entidad
+
             /^[a-zA-ZÀ-ÿ\00f1\00d1]+(\s*[a-zA-ZÀ-ÿ\00f1\00d1]*)*[a-zA-ZÀ-ÿ\00f1\00d1]{2,30}$/, //Patron para nombre
-            /^[a-zA-ZÀ-ÿ\00f1\00d1]+(\s*[a-zA-ZÀ-ÿ\00f1\00d1]*)*[a-zA-ZÀ-ÿ\00f1\00d1]{4,20}$/, //Patron del rol de persona
+            /^[a-zA-ZÀ-ÿ\00f1\00d1]+(\s*[a-zA-ZÀ-ÿ\00f1\00d1]*)*[a-zA-ZÀ-ÿ\00f1\00d1]{2,30}$/, //Patron para apellido paterno
+            /^[a-zA-ZÀ-ÿ\00f1\00d1]+(\s*[a-zA-ZÀ-ÿ\00f1\00d1]*)*[a-zA-ZÀ-ÿ\00f1\00d1]{2,30}$/, //Patron para apellido materno
+
+            /^[a-zA-ZÀ-ÿ\00f1\00d1]+(\s*[a-zA-ZÀ-ÿ\00f1\00d1]*)*[a-zA-ZÀ-ÿ\00f1\00d1]{4,20}$/, //Patron del cargo
             /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/, //Patron de correo 
             /^\d{9}$/, //Patron de celular
             /^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,16}$/ //Patron de contraseña
         ];
+
         let register_data = [ //Datos del registro
             req.body.ruc,
             req.body.entidad,
             req.body.ubicacion,
             req.body.rol_entidad,
             req.body.nombre,
+            req.body.apellido1,
+            req.body.apellido2,
             req.body.cargo,
             req.body.correo,
             req.body.celular,
@@ -42,21 +50,28 @@ controllers.REGISTRAR_CUENTA = (req, res) => {
             case -1: msg = 'Parece que el negocio ha sido registrado...'; break;
             case 0: msg = 'Ocurrio un error en los datos ingresados. Intentelo nuevamente.'; break;
             case 1: 
-                mysqlConnection.query('call post_usu_usuario(?,?,?,?,?,?,?,?,?,?,?)',
-                    [
-                        req.body.ruc,
-                        req.body.entidad,
-                        req.body.ubicacion,
-                        req.body.rol_entidad,
-                        req.body.nombre,
-                        req.body.cargo,
-                        req.body.correo,
-                        req.body.celular,
-                        req.body.telefono,
-                        req.body.contrasenia,
-                        process.env.XLR8
-                    ]);
-                msg = 'Registro exitoso.';     
+                mysqlConnection.query('call post_emp_empresa(?,?,?,?,?)',[ //Registra datos de la empresa
+                    req.body.ruc,
+                    req.body.entidad,
+                    req.body.ubicacion,
+                    req.body.rol_entidad,
+                    req.body.emp_foto
+                ]);
+
+                mysqlConnection.query('call post_usu_usuario(?,?,?,?,?,?,?,?,?,?,?)',[ //Registra datos del usuario
+                    req.body.nombre,
+                    req.body.ruc,
+                    req.body.apellido1,
+                    req.body.apellido2,
+                    req.body.cargo,
+                    req.body.correo,
+                    req.body.celular,
+                    req.body.telefono,
+                    req.body.contrasenia,
+                    process.env.XLR8,
+                    req.body.usu_foto
+                ]);
+                msg = 'Registro exitoso.';    
             break;
         };
 
