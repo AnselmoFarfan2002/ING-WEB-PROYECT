8e39211eefa2b9a7f8cd49376c5fa4cb3d39cd99
AnselmoFarfan2002 <anselmofarfan2002@gmail.com>
2022-12-17 18:12:29 -0500

    Reparación de registro y validación captcha incorporada.

--- sql-scripts/usuario.procedimientos.sql
+++ sql-scripts/usuario.procedimientos.sql
@@ -1,6 +1,5 @@
 use LOCAL_DB;
 
-DELIMITER $$ 
 drop procedure if exists get_usu_contrasenia;	-- obtiene la contraseña del usuario
 drop procedure if exists post_emp_empresa;		-- registra una empresa
 drop procedure if exists post_car_cargo;		-- registra un cargo
@@ -12,10 +11,13 @@ drop procedure if exists put_emp_photo;		-- actualiza foto de una empresa
 drop procedure if exists get_usu_usuario;		-- obtiene registro de un usuario desde el id
 drop procedure if exists get_usu_usuario_byEmail;	-- obtiene registro de usuario desde el email
 
+DELIMITER $$ 
 create procedure get_usu_contrasenia( email varchar(40), keyword varchar(25) ) begin
 	select cast( aes_decrypt( USU_CONTRASENIA, keyword ) as char ) as pass from USUARIO where USU_CORREO = email;
 end;
+$$
 
+DELIMITER $$
 create procedure post_emp_empresa( 
 	EMP_RUC varchar(11),
 	EMP_ENTIDAD varchar(30),
@@ -25,13 +27,17 @@ create procedure post_emp_empresa(
 ) begin
 	INSERT INTO EMPRESA VALUES (EMP_RUC, EMP_ENTIDAD, EMP_UBICACION, EMP_ROL_ENTIDAD, EMP_FOTO);
 end;
+$$
 
+DELIMITER $$
 create procedure post_car_cargo( 
+	idCargo tinyint unsigned ,
 	CAR_NOMBRE varchar(20)
 ) begin
-	INSERT INTO CARGO (CAR_NOMBRE) VALUES (CAR_NOMBRE);
-end;
+	INSERT INTO CARGO VALUES (idCargo, CAR_NOMBRE, 1);
+end; $$
 
+DELIMITER $$
 create procedure post_usu_usuario( 
 	USU_NOMBRE varchar(30),
     USU_EMPRESA varchar(11),
@@ -49,7 +55,9 @@ create procedure post_usu_usuario(
 		(USU_NOMBRE, USU_EMPRESA, USU_APELLIDO1, USU_APELLIDO2, USU_CARGO, USU_CORREO, USU_CELULAR, USU_TELEFONO, USU_CONTRASENIA, USU_FOTO)
 		VALUES (USU_NOMBRE, USU_EMPRESA, USU_APELLIDO1, USU_APELLIDO2, USU_CARGO, USU_CORREO, USU_CELULAR, USU_TELEFONO, aes_encrypt(USU_CONTRASENIA, keyword), USU_FOTO);
 end;
+$$
 
+DELIMITER $$
 create procedure put_usu_usuario(
 	_NOMBRE varchar(30),
     _APELLIDO1 varchar(30),
@@ -70,26 +78,29 @@ create procedure put_usu_usuario(
 		USU_TELEFONO 	= _TELEFONO
 	WHERE USU_ID = _ID;
 end;
+$$
 
+DELIMITER $$
 create procedure put_usu_contrasenia( userId int unsigned, pass varchar(16), keyword varchar(25) ) begin
 	UPDATE USUARIO SET USU_CONTRASENIA = aes_encrypt( pass, keyword ) WHERE USU_ID = userId;
-end;
+end;$$
 
+DELIMITER $$
 create procedure put_usu_photo( userId int unsigned, photo varchar(50) ) begin
 	UPDATE USUARIO SET USU_FOTO = photo WHERE USU_ID = userId;
-end;
+end;$$
 
+DELIMITER $$
 create procedure put_emp_photo( ruc varchar(11), photo varchar(50) ) begin
 	UPDATE EMPRESA SET EMP_FOTO = photo WHERE EMP_RUC = ruc;
-end;
+end;$$
 
+DELIMITER $$
 create procedure get_usu_usuario( userId int unsigned ) begin
 	SELECT USU_ID as id, EMP_UBICACION as ubicacionEmpresa, EMP_ENTIDAD as nombreEmpresa, USU_EMPRESA as rucEmpresa, USU_TELEFONO as telefono, USU_CELULAR as celular, USU_NOMBRE as nombre, USU_APELLIDO1 as apellido1, USU_APELLIDO2 as apellido2, CAR_NOMBRE as cargo, USU_CORREO as correo, USU_FOTO as foto FROM USUARIO 
     JOIN CARGO ON USU_CARGO = CAR_ID
     JOIN EMPRESA ON USU_EMPRESA = EMP_RUC WHERE USU_ID = userID;
-end;
-
-$$ 
+end;$$
 
 DELIMITER $$
 create procedure get_usu_usuario_byEmail( correo varchar(40) ) begin
--- src/controllers/module 1 - users accounts/registrador.js
+++ src/controllers/module 1 - users accounts/registrador.js
@@ -1,44 +1,74 @@
 const mysqlConnection = require( '../../config/db-connection' );
+const request = require('request');
 const controllers = {};
 
-controllers.REGISTRAR_CUENTA = (req, res) => {
-    let query = mysqlConnection.format('select validar_registro(?,?) as status', [req.body.correo, req.body.ruc]);
-    
-    mysqlConnection.query(query, (err, rows, ifield)=>{
-        let msg = ''; //Mensaje
-
-        switch(rows[0].status) {
-            case -1: msg = 'La empresa ya ha sido registrada.'; break;
-            case  0: msg = 'El correo ya ha sido registrado'; break;
-            case  1: 
-                mysqlConnection.query('call post_emp_empresa(?,?,?,?,?)',[ //Registra datos de la empresa
-                    req.body.ruc,
-                    req.body.entidad,
-                    req.body.ubicacion,
-                    req.body.rol_entidad,
-                    'defaultBusiness.jpg'
-                ]);
-
-                mysqlConnection.query('call post_usu_usuario(?,?,?,?,?,?,?,?,?,?,?)',[ //Registra datos del usuario
-                    req.body.nombre,
-                    req.body.ruc,
-                    req.body.apellido1,
-                    req.body.apellido2,
-                    req.body.cargo,
-                    req.body.correo,
-                    req.body.celCod + ' ' + req.body.celular, // '+51' + ' ' + '123456789'
-                    req.body.telefono,
-                    req.body.contrasenia,
-                    process.env.XLR8,
-                    'defaultUser.jpg'
-                ]);
-
-                msg = 'Registro exitoso.';    
-            break;
-        };
-
-        res.send({ status: rows[0].status, msg });
+const validarCaptcha = (req) => new Promise((resolve, reject) => {   
+    request(`https://www.google.com/recaptcha/api/siteverify?
+        secret=${process.env.XLR7}
+        &response=${req.body['g-recaptcha-response']}
+        &remoteip=${req.connection.remoteAddress}`, 
+    (err, res, body) => {
+        body = JSON.parse(body);
+        if(body.success !== undefined && !body.success) reject(err);
+        else resolve();
     });
+})
+
+controllers.REGISTRAR_CUENTA = (req, res) => {
+    validarCaptcha(req)
+    .then(() => {
+        let query = mysqlConnection.format('select validar_registro(?,?) as status', [req.body.correo, req.body.ruc]);
+    
+        mysqlConnection.query(query, (err, rows, ifield)=>{
+            let msg = ''; //Mensaje
+
+            switch(rows[0].status) {
+                case -1: msg = 'La empresa ya ha sido registrada.'; break;
+                case  0: msg = 'El correo ya ha sido registrado'; break;
+                case  1: 
+                    mysqlConnection.query('SELECT CAR_NOMBRE FROM CARGO', (err, rows) => new Promise((resolve, reject) => {
+                        let cargo = rows.indexOf(req.body.cargo);
+                        if(cargo === -1) {
+                            mysqlConnection.query('call post_car_cargo(?,?)', [rows.length + 1, req.body.cargo], (err) => {
+                                if(err) console.log(err)
+                                else resolve(rows.length + 1);
+                            })
+                        } else resolve(cargo + 1);
+                    }).then( idCargo => {
+                        mysqlConnection.query('call post_emp_empresa(?,?,?,?,?)',[ //Registra datos de la empresa
+                            req.body.ruc,
+                            req.body.entidad,
+                            req.body.ubicacion,
+                            req.body.rol_entidad,
+                            'defaultBusiness.jpg'
+                        ], err => new Promise((resolve, reject) => {
+                            if(err) reject(err);
+                            else resolve();
+                        }).then( () => {     
+                            mysqlConnection.query('call post_usu_usuario(?,?,?,?,?,?,?,?,?,?,?)',[ //Registra datos del usuario
+                                req.body.nombre,
+                                req.body.ruc,
+                                req.body.apellido1,
+                                req.body.apellido2,
+                                idCargo,
+                                req.body.correo,
+                                req.body.celCod + ' ' + req.body.celular, // '+51' + ' ' + '123456789'
+                                req.body.telefono,
+                                req.body.contrasenia,
+                                process.env.XLR8,
+                                'defaultUser.jpg'
+                            ]);
+                        }).catch( err => console.log(err) ));
+                    }));
+
+                    msg = 'Registro exitoso.';    
+                break;
+            };
+
+            res.send({ status: rows[0].status, msg });
+        });
+    })
+    .catch(() => {res.send({msg: 'Prueba de captcha fallida', status: -1})})
 };
 
 module.exports = controllers;
